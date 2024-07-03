de23072220689438b0d74e0444f46bc5888c52e9
Williams Barriquero <willbrr.dev@gmail.com>
2024-07-03 16:23:12 -0300

    OO

--- app.py
+++ app.py
@@ -1,211 +1,24 @@
-"""
-This script displays a menu of options for managing restaurants.
-"""
-import os
+from models.restaurante import Restaurante
 
-restaurantes = [
-    {'nome': 'Praça', 'categoria': 'Japonesa', 'ativo': False},
-    {'nome': 'Pizza Suprema', 'categoria': 'Pizza', 'ativo': True},
-    {'nome': 'Cantina', 'categoria': 'Italiana', 'ativo': False}
-]
+pizzaria_joao = Restaurante('Pizzaria do João', 'Pizzaria')
+sorveteria_maju = Restaurante('Sorveteria da Maju', 'Sorveteria')
+pastel_maju = Restaurante('Pastel da Maju', 'Pastelaria')
 
+pizzaria_joao.alterar_estado()
+pizzaria_joao.receber_avaliacao('João', 5)
+pizzaria_joao.receber_avaliacao('Maria', 4)
+pizzaria_joao.receber_avaliacao('José', 3)
 
-def exibir_nome_do_programa():
-    """
-    This function displays the name of the program.
-    """
-    print(
-        """
-███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗  ███████╗██╗░░░░░░█████╗░██╗░░░██╗░█████╗░██████╗░
-██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝  ██╔════╝██║░░░░░██╔══██╗██║░░░██║██╔══██╗██╔══██╗
-█████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░  █████╗░░██║░░░░░███████║╚██╗░██╔╝██║░░██║██████╔╝
-██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗  ██╔══╝░░██║░░░░░██╔══██║░╚████╔╝░██║░░██║██╔══██╗
-███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝  ██║░░░░░███████╗██║░░██║░░╚██╔╝░░╚█████╔╝██║░░██║
-╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  ╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
-        """
-    )
+sorveteria_maju.receber_avaliacao('João', 5)
+sorveteria_maju.receber_avaliacao('Maria', 4)
+sorveteria_maju.receber_avaliacao('José', 5)
 
-
-def exibir_opcoes():
-    """
-    Exibe as opções disponíveis para o usuário.
-    """
-    print('1. Cadastrar restaurante')
-    print('2. Listar restaurantes')
-    print('3. Ativar restaurante')
-    print('4. Sair\n')
-
-
-def finalizar_app():
-    """
-    Finalizes the app by displaying a subtitle.
-    """
-    exibir_subtitulo('Finalizar app')
-
-
-def voltar_ao_menu_principal():
-    """
-    Função que aguarda a entrada do usuário para voltar ao menu principal.
-    """
-    input('\nDigite uma tecla para voltar ao menu ')
-    main()
-
-
-def opcao_invalida():
-    """
-    Prints a message indicating that the option is invalid.
-    """
-    print('Opção inválida!\n')
-    voltar_ao_menu_principal()
-
-
-def exibir_subtitulo(texto):
-    """
-    Function to display a subtitle.
-
-    Parameters:
-    texto (str): The subtitle text to be displayed.
-
-    Returns:
-    None
-    """
-    os.system('clear')
-    line = '*' * len(texto)
-    print(line)
-    print(texto)
-    print(line)
-    print()
-
-
-def cadastrar_novo_restaurante():
-    """
-    Função para cadastrar um novo restaurante.
-
-    Solicita ao usuário o nome e a categoria do restaurante a ser cadastrado.
-    Cria um dicionário com as informações do restaurante e adiciona-o à lista de restaurantes.
-    Imprime uma mensagem de sucesso ao cadastrar o restaurante.
-    Chama a função voltar_ao_menu_principal para retornar ao menu principal.
-    """
-    exibir_subtitulo('Cadastro de novos restaurantes')
-
-    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    categoria_do_restaurante = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
-
-    restaurante = {
-        'nome': nome_do_restaurante,
-        'categoria': categoria_do_restaurante,
-        'ativo': False
-    }
-
-    restaurantes.append(restaurante)
-    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-
-    voltar_ao_menu_principal()
-
-
-def listar_restaurantes():
-    """
-    This function lists the restaurants.
-
-    It displays a table with the restaurant number, name, category, and state.
-    The table is printed to the console.
-
-    Parameters:
-    None
-
-    Returns:
-    None
-
-    ou pode usar ljust para alinhar
-    print(f"{'Nº'.ljust(3)}")
-    """
-
-    exibir_subtitulo('Listando restaurantes')
-
-    print(f"{'Nº':<3} | {'Nome':<20} | {'Categoria':<20} | {'Estado':<10}")
-    for index, restaurante in enumerate(restaurantes):
-        nome = restaurante['nome']
-        categoria = restaurante['categoria']
-        ativo = restaurante['ativo']
-
-        print(f'{index + 1:<3} | {nome:<20} | {categoria:<20} | {"Ativo" if ativo else "Inativo":<10}')
-
-
-def listando_restaurantes():
-    """
-    Lists all the restaurants.
-    """
-    listar_restaurantes()
-
-    voltar_ao_menu_principal()
-
-
-def alterar_estado_do_restaurante():
-    """
-    Altera o estado (ativo ou inativo) de um restaurante selecionado.
-
-    Exibe uma lista de restaurantes disponíveis e solicita ao usuário que digite o número do restaurante que deseja alterar o estado.
-    Em seguida, altera o estado do restaurante selecionado e exibe uma mensagem de sucesso.
-    Caso o restaurante não seja encontrado, exibe uma mensagem informando que o restaurante não foi encontrado.
-
-    """
-    exibir_subtitulo('Alterando status do restaurante')
-
-    listar_restaurantes()
-
-    restaurante_selecionado = int(input('\nDigite o número do restaurante que deseja alterar o estado: ')) - 1
-    restaurante_encontrado = False
-
-    for index, restaurante in enumerate(restaurantes):
-        if restaurante_selecionado == index:
-            restaurante_encontrado = True
-            restaurante['ativo'] = not restaurante['ativo']
-            mensagem = f'O restaurante {restaurante["nome"]} foi desativado com sucesso!' \
-                if not restaurante['ativo'] else f'O restaurante {restaurante["nome"]} foi ativado com sucesso!'
-            print(mensagem)
-
-    if not restaurante_encontrado:
-        print('Restaurante não encontrado!')
-
-    voltar_ao_menu_principal()
-
-
-def escolher_opcao():
-    """
-    Função responsável por solicitar ao usuário que escolha uma opção e executar a ação correspondente.
-
-    Raises:
-        RuntimeError: Se ocorrer algum erro ao escolher a opção.
-
-    """
-    try:
-        opcao_escolhida = int(input('Escolha uma opção: '))
-
-        if opcao_escolhida == 1:
-            cadastrar_novo_restaurante()
-        elif opcao_escolhida == 2:
-            listando_restaurantes()
-        elif opcao_escolhida == 3:
-            alterar_estado_do_restaurante()
-        elif opcao_escolhida == 4:
-            finalizar_app()
-        else:
-            opcao_invalida()
-    except Exception as e:
-        raise RuntimeError("Erro ao escolher opção: ", e) from e
+pastel_maju.receber_avaliacao('João', 4)
+pastel_maju.receber_avaliacao('Maria', 3)
 
 
 def main():
-    """
-    This is the main function of the program.
-    It clears the screen, displays the program name,
-    shows the available options, and allows the user
-    to choose an option.
-    """
-    os.system('clear')
-    exibir_nome_do_programa()
-    exibir_opcoes()
-    escolher_opcao()
+    Restaurante.listar_restaurantes()
 
 
 if __name__ == '__main__':
--- models/restaurante.py
+++ models/restaurante.py
@@ -1,3 +1,6 @@
+from models.avaliacao import Avaliacao
+
+
 class Restaurante:
     restaurantes = []
 
@@ -5,6 +8,7 @@ class Restaurante:
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -12,13 +16,14 @@ class Restaurante:
 
     @classmethod
     def listar_restaurantes(cls):
-        print(f"{'Nº':<3} | {'Nome':<20} | {'Categoria':<20} | {'Estado':<10}")
+        print(f"{'Nº':<3} | {'Nome':<20} | {'Categoria':<20} | {'Avaliação':<20} | {'Estado':<10}")
         for index, restaurante in enumerate(cls.restaurantes):
             nome = restaurante._nome
             categoria = restaurante._categoria
             ativo = restaurante.ativo
+            media = restaurante.media_avaliacao
 
-            print(f'{index + 1:<3} | {nome:<20} | {categoria:<20} | {ativo:<10}')
+            print(f'{index + 1:<3} | {nome:<20} | {categoria:<20} | {media:<20} | {ativo:<10}')
 
     @property
     def ativo(self):
@@ -27,9 +32,19 @@ class Restaurante:
     def alterar_estado(self):
         self._ativo = not self._ativo
 
+    def receber_avaliacao(self, cliente, nota):
+        if 0 > nota >= 5:
+            raise ValueError('Nota inválida. A nota deve ser entre 0 e 5.')
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
 
-r = Restaurante('Pizzaria do João', 'Pizzaria')
-r.alterar_estado()
+    @property
+    def media_avaliacao(self) -> (str | float):
+        if not self._avaliacao:
+            return '-'
 
-r2 = Restaurante('Pizzaria do João 2', 'Pizzaria')
-r.listar_restaurantes()
+        soma_notas = sum([avaliacao._nota for avaliacao in self._avaliacao])
+        qtd_notas = len(self._avaliacao)
+        media = round(soma_notas / qtd_notas, 1)
+
+        return media
